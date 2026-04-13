df5390a2ae72daa1f4d184514eca7699356777de
baboribo <inertia_mailer_0c@icloud.com>
2026-04-13 11:58:25 +0900

    Enhance now playing UI with improved layout and styling; update text elements for clarity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,47 +36,54 @@
 </script>
 
 {#if pfView}
-  <div class="main-container">
-      <img class="avatar" src="https://avatars.githubusercontent.com/u/71020988?v=4" alt="?" />
-      <h2>바보리보</h2>
-      <p class="txt-secondary">여러가지 시도를 좋아해요.</p>
-  </div>
+    <div class="main-container">
+          <img class="avatar" src="https://avatars.githubusercontent.com/u/71020988?v=4" alt="?" />
+          <h2>y^7x</h2>
+          <p class="txt-secondary">설명</p>
+    </div>
 {/if}
 
-<h2>현재 듣는 곡</h2>
+<h3 style="padding: 8px 13px;">현재 듣는 곡</h3>
 {#if nowPlaying}
-  {#if nowPlaying.is_playing}
-    <div>
-      <img 
-        src={nowPlaying.item.album.images[0].url} 
-        alt={nowPlaying.item.name} 
-        width="80"
-      />
-      <div>
-        <h4><a href={nowPlaying.item.album.external_urls.spotify} target="_blank" rel="noopener noreferrer nofollow">{nowPlaying.item.name}</a></h4>
-        
-        <p class="txt-secondary">
-          {nowPlaying.item.artists.map((a: any) => a.name).join(', ')}
-        </p>
-        <div class="progress-bg">
-        <div 
-            class="progress-bar" 
-            style="width: {(nowPlaying.progress_ms / nowPlaying.item.duration_ms) * 100}%"
-        ></div>
+    {#if nowPlaying.is_playing}
+        <div class="now-playing-container">
+            <img 
+              src={nowPlaying.item.album.images[0].url} 
+              alt={nowPlaying.item.name} 
+              class="now-playing-cover"
+            />
+            <div class="now-playing-info">
+                <div class="now-playing-txts">
+                    <h4>
+                        <a href={nowPlaying.item.album.external_urls.spotify} target="_blank" rel="noopener noreferrer nofollow">{nowPlaying.item.name}</a>
+                    </h4>
+                    <p class="txt-secondary">
+                        {nowPlaying.item.artists.map((a: any) => a.name).join(', ')}
+                    </p>
+                </div>
+                
+                <div class="now-playing-progress-frame">
+                    <div class="progress-bg">
+                    <div 
+                        class="progress-bar" 
+                        style="width: {(nowPlaying.progress_ms / nowPlaying.item.duration_ms) * 100}%"
+                    ></div>
+                    </div>
+                    <div class="now-playing-progress-mstxt">
+                        <span class="txt-secondary-sub">
+                            {Math.floor(nowPlaying.progress_ms / 1000 / 60)}:{String(Math.floor((nowPlaying.progress_ms / 1000) % 60)).padStart(2, '0')} 
+                        </span>
+                        <span class="txt-secondary-sub">
+                            {Math.floor(nowPlaying.item.duration_ms / 1000 / 60)}:{String(Math.floor((nowPlaying.item.duration_ms / 1000) % 60)).padStart(2, '0')} 
+                        </span>
+                    </div>
+                </div>
+                <!-- <span class="txt-secondary-sub">Spotify에서 듣는 중</span> -->
+            </div>
         </div>
-        
-        <span class="txt-secondary-sub">
-            {Math.floor(nowPlaying.progress_ms / 1000 / 60)}:{String(Math.floor((nowPlaying.progress_ms / 1000) % 60)).padStart(2, '0')} 
-            / 
-            {Math.floor(nowPlaying.item.duration_ms / 1000 / 60)}:{String(Math.floor((nowPlaying.item.duration_ms / 1000) % 60)).padStart(2, '0')}
-        </span>
-
-        <span class="txt-secondary-sub">Spotify에서 듣는 중</span>
-      </div>
-    </div>
-  {:else}
+    {:else}
     <p>지금은 노래를 듣고 있지 않아요.</p>
-  {/if}
+{/if}
 {:else}
   <p>데이터 불러오는 중...</p>
 {/if}
